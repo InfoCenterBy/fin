8064df17039ee5ce13ab6b1b7321fdf26c62eee5
InfoCenterBy <94043508+SVENmods@users.noreply.github.com>
2022-11-22 17:51:18 +0300

    almost done services and some visual fix
    Bug: bg on FAQ block btns

--- #src/js/app.js
+++ #src/js/app.js
@@ -113,3 +113,53 @@ function erase() {
 document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
   if(textArray.length) setTimeout(type, newTextDelay + 250);
 });
+
+
+
+// const accordionBtn = document.querySelectorAll(".item-accordion__button");
+
+// for (let i = 0; i < accordionBtn.length; i++) {
+//   accordionBtn[i].addEventListener("click", function(){
+//     let accordion = document.getElementsByClassName("accordion-collapse")
+//     for(let z = 0; z< accordion.length; z++){
+//       if(accordion[i].classList.contains("show")){
+//         console.log(this.classList)
+//       }
+//       else{
+
+//       }
+//     }
+//   })
+  
+// }
+    // accord.addEventListener('click', function (event) {  
+    //     event.preventDefault();
+    //     console.log("res")
+    // }, false);
+
+const tabs = document.querySelectorAll(".tab")
+// получаем массив всех блоков с содержимым вкладок
+
+const contents = document.querySelectorAll(".content")
+// запускаем цикл для каждой вкладки и добавляем на неё событие
+
+for (let i = 0; i < tabs.length; i++) {
+  tabs[i].addEventListener("click", ( event ) => {
+    event.preventDefault()
+    // сначала нам нужно удалить активный класс именно с вкладок
+    let tabsChildren = event.target.parentElement.children;
+    for (let t = 0; t < tabsChildren.length; t++) {
+      tabsChildren[t].classList.remove("tab-active")
+    }
+    // добавляем активный класс
+    tabs[i].classList.add("tab-active")
+    // теперь нужно удалить активный класс с блоков содержимого вкладок
+    let tabContentChildren = event.target.parentElement.nextElementSibling.children;
+    for (let c = 0; c < tabContentChildren.length; c++) {
+      tabContentChildren[c].classList.remove("content-active")
+    }
+    // добавляем активный класс
+    contents[i].classList.add("content-active")
+  });
+}
+
