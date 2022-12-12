35da59f2e65ade31911455b604c6d2a715148112
InfoCenterBy <94043508+SVENmods@users.noreply.github.com>
2022-12-12 17:55:03 +0300

    Made 404 page and about

--- fin/js/script.js
+++ fin/js/script.js
@@ -263,72 +263,6 @@ let bodyLock = (delay = 500) => {
   }
 }
 
-
-const typedTextSpan = document.querySelector(".typed-text");
-const cursorSpan = document.querySelector(".cursor");
-
-const textArray = ["Профессиональные консультации", "Ведение бухгалтерии", "Расчет заработной платы"];
-const typingDelay = 100;
-const erasingDelay = 50;
-const newTextDelay = 1500; // Delay between current and next text
-let textArrayIndex = 0;
-let charIndex = 0;
-
-function type() {
-  if (charIndex < textArray[textArrayIndex].length) {
-    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
-    charIndex++;
-    setTimeout(type, typingDelay);
-  }
-  else {
-    cursorSpan.classList.remove("typing");
-    setTimeout(erase, newTextDelay);
-  }
-}
-
-function erase() {
-  if (charIndex > 0) {
-    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
-    charIndex--;
-    setTimeout(erase, erasingDelay);
-  }
-  else {
-    cursorSpan.classList.remove("typing");
-    textArrayIndex++;
-    if(textArrayIndex>=textArray.length) textArrayIndex=0;
-    setTimeout(type, typingDelay + 1100);
-  }
-}
-
-document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
-  if(textArray.length) setTimeout(type, newTextDelay + 250);
-});
-
-
-
-const accordionBtn = document.querySelectorAll(".item-accordion__button");
-
-for (let i = 0; i < accordionBtn.length; i++) {
-  accordionBtn[i].addEventListener("click", function(){
-    let accordion = document.getElementsByClassName("accordion-collapse")
-    for(let z = 0; z< accordion.length; z++){
-      if(accordion[i].classList.contains("show")){
-        console.log(this.classList)
-      }
-      else{
-
-      }
-    }
-  })
-  
-}
-    accord.addEventListener('click', function (event) {  
-        event.preventDefault();
-        console.log("res")
-    }, false);
-
 const tabs = document.querySelectorAll(".tab")
 // получаем массив всех блоков с содержимым вкладок
 
@@ -354,4 +288,98 @@ for (let i = 0; i < tabs.length; i++) {
     contents[i].classList.add("content-active")
   });
 }
+
+
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["Профессиональные консультации", "Ведение бухгалтерии", "Расчет заработной платы"];
+const typingDelay = 100;
+const erasingDelay = 50;
+const newTextDelay = 1500; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+// function type() {
+//   if (charIndex < textArray[textArrayIndex].length) {
+//     if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+//     typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+//     charIndex++;
+//     setTimeout(type, typingDelay);
+//   }
+//   else {
+//     cursorSpan.classList.remove("typing");
+//     setTimeout(erase, newTextDelay);
+//   }
+// }
+
+// function erase() {
+//   if (charIndex > 0) {
+//     if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+//     typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+//     charIndex--;
+//     setTimeout(erase, erasingDelay);
+//   }
+//   else {
+//     cursorSpan.classList.remove("typing");
+//     textArrayIndex++;
+//     if(textArrayIndex>=textArray.length) textArrayIndex=0;
+//     setTimeout(type, typingDelay + 1100);
+//   }
+// }
+
+// document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+//   if(textArray.length) setTimeout(type, newTextDelay + 250);
+// });
+
+
+
+const accordionBtn = document.querySelectorAll(".item-accordion__button");
+
+for (let i = 0; i < accordionBtn.length; i++) {
+  accordionBtn[i].addEventListener("click", function(){
+    let accordion = document.getElementsByClassName("accordion-collapse")
+    for(let z = 0; z< accordion.length; z++){
+      if(accordion[i].classList.contains("show")){
+        console.log(this.classList)
+      }
+      else{
+
+      }
+    }
+  })
+  
+}
+
+
+let swiper = new Swiper(".team-swiper", {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  breakpoints: {
+    640:{
+      slidesPerView: 3
+    },
+    480:{
+      slidesPerView: 2
+    },
+    320:{
+      slidesPerView: 1
+    },
+  }
+});
+
+accord.addEventListener('click', function (event) {  
+    event.preventDefault();
+    console.log("res")
+}, false);
+
 
