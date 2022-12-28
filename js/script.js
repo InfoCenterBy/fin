3e371e320b4fb3686f5957e3015e55344451169a
InfoCenterBy <94043508+SVENmods@users.noreply.github.com>
2022-12-28 09:30:55 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -446,10 +446,11 @@ for(let i = link.length - 1; i >= 0; i-- ){
     document.querySelector(".menu-header__item").classList.remove("hover-menu");
     setTimeout(()=>{
       document.querySelector(".menu-header__item").classList.add("hover-menu");
-    },400)
+    },1000)
     // console.log()
   }, false)
 }
+
 
 
 
